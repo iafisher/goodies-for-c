aead60acf1754d6fb1d660a58f3b60e7e215c256
Ian Fisher <iafisher@protonmail.com>
2018-07-13 07:56:38 -0400

    Begin implementing qstring

---
+++ qstring.c
@@ -0,0 +1,242 @@
+/* Implementation of the qstring library. See qstring.h for API documentation.
+ *
+ * Author:  Ian Fisher (iafisher@protonmail.com)
+ * Version: July 2018
+ */
+
+#include <stdlib.h>
+#include <string.h>
+
+qstring qstring_new(const char* cs) {
+    return qstring_new_buffer(cs, strlen(cs));
+}
+
+qstring qstring_new_buffer(const char* buffer, size_t n) {
+    qstring ret = {.n = 0, .data = malloc(n + 1)};
+    if (ret.data == NULL) {
+        return ret;
+    }
+    memcpy(ret.data, src, n);
+    ret.data[n] = '\0';
+    ret.len = n;
+    return ret;
+}
+
+qstring qstring_repeat(char c, size_t n) {
+    qstring ret = {.n = 0, .data = malloc(n + 1)};
+    if (ret.data == NULL) {
+        return ret;
+    }
+    for (size_t i = 0; i < n; i++) {
+        ret.data[i] = c;
+    }
+    ret.data[n] = '\0';
+    ret.len = n;
+    return ret;
+}
+
+qstring qliteral_new(const char* cs) {
+    qstring ret = {.n = strlen(cs), .data = cs};
+    return ret;
+}
+
+void qstring_cleanup(qstring qs) {
+    free(qs.data);
+}
+
+qstring qstring_copy(qstring qs) {
+    return qstring_new_buffer(qs.data, qs.len);
+}
+
+qstring qstring_substr(qstring qs, size_t start, size_t n) {
+    if (start >= qs.len) {
+        return qstring_new("");
+    }
+    if (start + n >= qs.len) {
+        n = qs.len - start;
+    }
+    return qstring_new_buffer(qs.data + start, n);
+}
+
+qstring qstring_remove(qstring qs, size_t start, size_t n) {
+    if (start >= qs.len) {
+        return qstring_copy(qs);
+    }
+    if (start + n >= qs.len) {
+        n = qs.len - start;
+    }
+    qstring ret = {.n = 0, .data = malloc(qs.len - n)};
+    if (ret.data == NULL) {
+        return ret;
+    }
+    memcpy(ret.data, qs.data, start);
+    memcpy(ret.data + start, qs.data + start + n, qs.len - n);
+    ret.len = qs.len - n;
+    return ret;
+}
+
+qstring qstring_concat(qstring qs1, qstring qs2) {
+    qstring ret = {.n = 0, .data = malloc(qs1.len + qs2.len + 1)};
+    if (ret.data == NULL) {
+        return ret;
+    }
+    memcpy(ret.data, qs1.data, qs1.len);
+    memcpy(ret.data + qs1.len, qs2.data, qs2.len + 1);
+    ret.len = qs1.len + qs2.len;
+    return ret;
+}
+
+qstring qstring_format(qstring fmtstr, ...) {
+    // TODO
+}
+
+qstring qstring_replace_all(qstring qs, qstring before, qstring after) {
+    size_t count = qstring_count(qs, before);
+    if (count == 0) {
+        return qstring_copy(qs);
+    }
+    size_t newlen = qs.len + count * (after.len - before.len);
+    qstring ret = {.len = 0, .data = malloc(newlen + 1)};
+    if (ret.data == NULL) {
+        return ret;
+    }
+    size_t index = 0;
+    // TODO: Finish this with qstring_find_in
+}
+
+static qstring replace_substr(qstring qs, size_t start, size_t n,
+    qstring replacing) {
+    size_t newlen = qs.len - n + replacing.len;
+    qstring ret = {.len = 0, .data = malloc(newlen + 1)};
+    if (ret.data == NULL) {
+        return ret;
+    }
+    memcpy(ret.data, qs.data, start);
+    memcpy(ret.data + start, replacing.data, replacing.len);
+    memcpy(ret.data + start + replacing.len, qs.data + start + n,
+        qs.len - index - n + 1);
+    ret.len = newlen;
+    return ret;
+}
+
+qstring qstring_replace_first(qstring qs, qstring before, qstring after) {
+    size_t index = qstring_find(qs, before);
+    if (index == qs.len) {
+        return qstring_copy(qs);
+    }
+    return replace_substr(qs, index, before.len, after);
+}
+
+qstring qstring_replace_last(qstring qs, qstring before, qstring after) {
+    size_t index = qstring_rfind(qs, before);
+    if (index == qs.len) {
+        return qstring_copy(qs);
+    }
+    return replace_substr(qs, index, before.len, after);
+}
+
+size_t qstring_find(qstring qs, qstring datum) {
+    return qstring_find_in(qs, datum, 0, qs.len);
+}
+
+size_t qstring_find_in(qstring qs, qstring datum, size_t start, size_t n) {
+    if (start >= qs.len) {
+        return qs.len;
+    }
+    if (start + n >= qs.len) {
+        n = qs.len - start;
+    }
+    /* This check is not just an optimization: it's necessary to prevent memcmp
+     * from examining invalid memory.
+     */
+    if (datum.len > n) {
+        return qs.len;
+    }
+
+    for (size_t i = start; i < start + n; i++) {
+        if (memcmp(qs.data + i, datum.data, datum.len) == 0) {
+            return i;
+        }
+    }
+    return qs.len;
+}
+
+size_t qstring_rfind(qstring qs, qstring datum) {
+    return qstring_rfind_in(qs, datum, 0, qs.len);
+}
+
+size_t qstring_rfind_in(qstring qs, qstring datum, size_t start, size_t n) {
+    if (start >= qs.len) {
+        return qs.len;
+    }
+    if (start + n >= qs.len) {
+        n = qs.len - start;
+    }
+    /* This check is not just an optimization: it's necessary to prevent memcmp
+     * from examining invalid memory.
+     */
+    if (datum.len > n) {
+        return qs.len;
+    }
+
+    for (size_t i = start; i < start + n; i++) {
+        size_t ri = (start + n) - (i + 1);
+        if (memcmp(qs.data + ri, datum.data, datum.len) == 0) {
+            return ri;
+        }
+    }
+    return qs.len;
+}
+
+size_t qstring_count(qstring qs, qstring datum) {
+    /* Make sure to test weird cases like count("aaa", "aa") == 1 */
+    if (datum.len > qs.len) {
+        return 0;
+    }
+
+    size_t count = 0;
+    for (size_t i = 0; i < qs.len; i++) {
+        if (memcp(qs.data + i, datum.data, datum.len) == 0) {
+            count++;
+            i += datum.len;
+        }
+    }
+    return count;
+}
+
+bool qstring_startswith(qstring qs, qstring prefix) {
+    return qs.len >= prefix.len && memcmp(qs.data, prefix.data, prefix.len) == 0;
+}
+
+bool qstring_endswith(qstring qs, qstring endswith) {
+    return qs.len >= prefix.len &&
+        memcmp(qs.data + (qs.len - prefix.len), prefix.data, prefix.len) == 0;
+}
+
+qstring qstring_lstrip(qstring qs, qstring to_strip) {
+    size_t nprefix = 0;
+    while (memchr(to_strip.data, qs.data[nprefix], to_strip.len) != NULL) {
+        nprefix++;
+    }
+    return qstring_substr(qs, nprefix, qs.len - nprefix);
+}
+
+qstring qstring_rstrip(qstring qs, qstring to_strip) {
+    size_t nsuffix = 0;
+    while (memchr(to_strip.data, qs.data[nsuffix], to_strip.len) != NULL) {
+        nsuffix++;
+    }
+    return qstring_substr(qs, 0, qs.len - nsuffix);
+}
+
+qstring qstring_strip(qstring qs, qstring to_strip) {
+    size_t nsuffix = 0;
+    while (memchr(to_strip.data, qs.data[nsuffix], to_strip.len) != NULL) {
+        nsuffix++;
+    }
+    size_t nprefix = 0;
+    while (memchr(to_strip.data, qs.data[nprefix], to_strip.len) != NULL) {
+        nprefix++;
+    }
+    return qstring_substr(qs, nprefix, qs.len - nprefix - nsuffix);
+}
--- qstring.h
+++ qstring.h
@@ -41,7 +41,8 @@
  *
  * If a qstring method returns a qstring, then that qstring is newly-allocated
  * from the heap, does not share data with any of the parameters, and must be
- * passed to qstring_cleanup to free memory.
+ * passed to qstring_cleanup to free memory. If allocation fails, then a
+ * qstring with a length of 0 and a NULL data field is returned.
  *
  *
  * Author:  Ian Fisher (iafisher@protonmail.com)
@@ -51,18 +52,22 @@
 #ifndef QSTRING_H
 #define QSTRING_H
 
+/* TODO: Add a qrange type? */
+
 typedef struct {
     /* Both fields are considered public and read-only. */
+
+    /* The number of bytes in the data field, excluding the null terminator. */
     size_t len;
-    /* This character array is null-terminated, so it can be used with any
+    /* This character array is null-terminated, so it can be passed to any
        standard C function. */
     const char* data;
 } qstring;
 
 
 /**
- * Return a stack-allocated qstring struct whose data field is a heap-allocated
- * copy of the string parameter, which must be null-terminated.
+ * Return a qstring containing a heap-allocated copy of the string parameter,
+ * which must be null-terminated.
  *
  * The returned qstring must eventually be passed to qstring_cleanup to avoid a
  * memory leak.
@@ -70,11 +75,11 @@ typedef struct {
 qstring qstring_new(const char*);
 
 /**
- * Return a stack-allocated qstrng struct whose data field is a heap-allocated
- * copy of the first `n` bytes of the string parameter. The parameter may
- * contain null bytes and does not need to be null-terminated, although the
- * data field in the returned qstring will be. The null terminator will be
- * inserted after the `n` characters of the buffer, so
+ * Return a qstring containing a heap-allocated copy of the first `n` bytes of
+ * the string parameter. The parameter may contain null bytes and does not need
+ * to be null-terminated, although the data field in the returned qstring will
+ * be. The null terminator will be inserted after the characters from the
+ * buffer, so
  *
  *   qstring_new_buffer("hello\0", 6)
  *
@@ -83,13 +88,19 @@ qstring qstring_new(const char*);
 qstring qstring_new_buffer(const char*, size_t n);
 
 /**
- * Return a stack-allocated qstring struct whose data field is set to the string
- * parameter, which must be null-terminated.
+ * Return a stack-allocated qstring struct whose data field is a heap-allocated
+ * buffer with `n` copies of the character `c`.
+ */
+qstring qstring_repeat(char c, size_t n);
+
+/**
+ * Return a stack-allocated qstring struct whose data field is set to the
+ * string parameter, which must be null-terminated.
  *
  * The returned qstring should NOT be passed to qstring_cleanup.
  *
- * If you are unsure whether to use qstring_new or qliteral_new, please read the
- * comment at the top of this module. If in doubt, choose qstring_new.
+ * If you are unsure whether to use qstring_new or qliteral_new, please read
+ * the comment at the top of this module. If in doubt, choose qstring_new.
  */
 qstring qliteral_new(const char*);
 
@@ -105,83 +116,91 @@ void qstring_cleanup(qstring);
 qstring qstring_copy(qstring);
 
 /**
- * Copy a substring of `n` characters of the qstring parameter, starting at
- * index `start`. If `start` is past the end of the string, the empty string
- * is returned. If `start` is within bounds but `start+n` is not, then a copy
- * of the substring from `start` to the end of string is returned.
+ * Copy a substring of `n` characters of `qs`, starting at index `start`. If
+ * `start` is out of bounds, the empty string is returned. If `start` is within
+ * bounds but `start+n` is not, then a copy of the substring from `start` to
+ * the end of string is returned.
  */
-qstring qstring_substr(qstring, size_t start, size_t n);
+qstring qstring_substr(qstring qs, size_t start, size_t n);
 
 /**
- * Return the first parameter with the substring removed. The `start` and `n`
- * parameters have the same meaning as with qstring_substr.
+ * Return `qs` with the substring removed. If `start` is out of bounds then a
+ * copy of the original qstring is returned. If `start + n` is out of bounds
+ * (but `start` isn't) then all bytes from `start` to the end of `qs` are
+ * removed.
  */
-qstring qstring_remove(qstring, size_t start, size_t n);
+qstring qstring_remove(qstring qs, size_t start, size_t n);
 
 /**
- * Concatenate the two qstrings together.
+ * Concatenate the two qstrings.
  */
 qstring qstring_concat(qstring, qstring);
 
 /**
- * Format the format string parameter with the arguments, as printf does.
+ * Insert the arguments into `fmtstr`, a format string as in printf.
  */
 qstring qstring_format(qstring fmtstr, ...);
 
 /**
- * Replace all instances of the second qstring in the first.
+ * Replace all non-overlapping instances of `before` with `after` in `qs`.
  */
-qstring qstring_replace_all(qstring, qstring);
+qstring qstring_replace_all(qstring qs, qstring before, qstring after);
 
 /**
- * Replace the first instance of the second qstring in the first.
+ * Replace the first instance of `before` with `after` in `qs`.
  */
-qstring qstring_replace_first(qstring, qstring);
+qstring qstring_replace_first(qstring qs, qstring before, qstring after);
 
 /**
- * Replace the last instance of the second qstring in the first.
+ * Replace the last instance of `before` with `after` in `qs`.
  */
-qstring qstring_replace_last(qstring, qstring);
+qstring qstring_replace_last(qstring qs, qstring before, qstring after);
 
 /**
- * Return the first instance of the second qstring in the first. If the second
- * is not found, then the length of the first qstring is returned.
+ * Return the index of the first instance of `datum` in `qs`. If `datum` is not
+ * found, then `qs.len` is returned.
  */
-size_t qstring_find(qstring, qstring);
+size_t qstring_find(qstring qs, qstring datum);
 
 /**
- * Return the last instance of the second qstring in the first. If the second
- * is not found, then the length of the first is returned.
+ * Return the first instance of `datum` in the specified substring of `qs`.
  */
-size_t qstring_rfind(qstring, qstring);
+size_t qstring_find_in(qstring qs, qstring datum, size_t start, size_t n);
 
 /**
- * Return the number of occurrences of the second qstring in the first.
+ * Return the index of the last instance of `datum` in `qs`. If `datum` is not
+ * found, then `qs.len` is returned.
  */
-size_t qstring_count(qstring, qstring);
+size_t qstring_rfind(qstring qs, qstring datum);
 
 /**
- * Return true if the first qstring starts with the second.
- *
- * qstring_startswith(qliteral_new("abcdef"), qliteral_new("abc")) == TRUE
+ * Return the last instance of `datum` in the specified substring of `qs`.
  */
-bool qstring_startswith(qstring, qstring);
+size_t qstring_rfind_in(qstring datum, qstring qs, size_t start, size_t n);
 
 /**
- * Return true if the first qstring ends with the second.
- *
- * qstring_endswith("abcdef", "def") == TRUE
+ * Return the number of non-overlapping occurrences of `datum` in `qs`.
  */
-bool qstring_endswith(qstring, qstring);
+size_t qstring_count(qstring qs, qstring datum);
 
 /**
- * Remove characters from the beginning of the first qstring until the first
- * character not in `to_strip` is encountered. The order of characters in the
- * second parameter does not matter.
+ * Return true if `qs` starts with `prefix`.
+ */
+bool qstring_startswith(qstring qs, qstring prefix);
+
+/**
+ * Return true if `qs` ends with `suffix`.
+ */
+bool qstring_endswith(qstring qs, qstring suffix);
+
+/**
+ * Remove characters from the beginning of `qs` until the first character not
+ * in `to_strip` is encountered. The order of characters in `to_strip` does
+ * not matter.
  *
  * qstring_lstrip("abbac", "ab") == "c"
  */
-qstring qstring_lstrip(qstring, qstring to_strip);
+qstring qstring_lstrip(qstring qs, qstring to_strip);
 
 /**
  * Same as qstring_lstrip, except from the end instead of the beginning.
@@ -189,16 +208,16 @@ qstring qstring_lstrip(qstring, qstring to_strip);
 qstring qstring_rstrip(qstring, qstring to_strip);
 
 /**
- * Equivalent to qstring_lstrip(qstring_rstrip(qs, to_strip), to_strip)
+ * Equivalent to qstring_lstrip(qstring_rstrip(qs, to_strip), to_strip), but
+ * more efficient.
  */
 qstring qstring_strip(qstring, qstring to_strip);
 
 /**
- * These methods are the same at their equivalent qstring_strip methods, with
- * the constant string " \t\n\v\f\r" supplied as the second parameter.
+ * Convenience macros for stripping whitespace.
  */
-qstring qstring_rstrip_ws(qstring);
-qstring qstring_lstrip_ws(qstring);
-qstring qstring_strip_ws(qstring);
+#define qstring_rstrip_ws(qs) qstring_rstrip(qs, " \t\n\r\v\f")
+#define qstring_lstrip_ws(qs) qstring_lstrip(qs, " \t\n\r\v\f")
+#define qstring_strip_ws(qs)  qstring_strip(qs, " \t\n\r\v\f")
 
 #endif
